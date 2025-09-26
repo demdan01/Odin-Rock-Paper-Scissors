c1f9d8f15ab8d66161df8c939c1153f5749ea4e3
demdan01 <145459294+demdan01@users.noreply.github.com>
2025-09-26 16:58:26 +0200

    Add functions for generating random number Add functions for computer choice

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+
+function getRandomIntBetween(min,max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function getComputerChoice() {
+    const choice = getRandomIntBetween(1,3);
+    
+    switch(choice) {
+  case 1:
+    return "ROCK";
+  case 2:
+    return "PAPER";
+  case 3:
+    return "SCISSORS";
+    }
+}
+
